cd5a04b36ce32d397e7aac98ab92b6f8a0442744
jejee <94217199+AjaTiftaUmara@users.noreply.github.com>
2021-12-01 14:51:10 +0700

    Add files via upload

--- src/main/java/Method/Minuman.java
+++ src/main/java/Method/Minuman.java
@@ -1,40 +1,54 @@
 package Method;
 
-public class Minuman{
-    private String ukuran;
-    private String suhu;
-    private boolean cafein;
+import java.util.ArrayList;
 
-    public Minuman(String nama, int harga, String jenis, String tag, String negara){
-        this.ukuran = ukuran;
-        this.suhu = suhu;
-        this.cafein = cafein;
-
+public class Minuman {
+    private String nama;
+    private int harga;
+    private boolean dingin;
+    private boolean caffein;
+    private final ArrayList<String> tag;
+    private String negara;
+    
+    /**
+     * 
+     * @param nama Nama minuman
+     * @param harga Harga minuman
+     * @param dingin Dingin
+     * @param caffein Mengandung kafein
+     * @param tag Tag
+     * @param negara Asal negara
+     */
+    public Minuman(String nama, int harga, boolean dingin, boolean caffein, ArrayList<String> tag, String negara){
+        this.nama = nama;
+        this.harga = harga;
+        this.dingin = dingin;
+        this.caffein = caffein;
+        this.tag = tag;
+        this.negara = negara;
     }
 
-    public String setUkuran(){
-        return this.ukuran;
+    public String getNama() {
+        return this.nama;
     }
 
-    public String setSuhu(){
-        return this.suhu;
+    public int getHarga() {
+        return this.harga;
+    }
+    
+    public boolean getDingin() {
+        return this.dingin;
     }
 
-    public boolean setCafein(){
-        return this.cafein;
+    public boolean getCaffein() {
+        return this.caffein;
     }
 
-    public String getUkuran(){
-        return this.ukuran;
+    public String getTag(int index) {
+        return this.tag.get(index);
     }
 
-    public String getSuhu(){
-        return this.suhu;
-
+    public String getNegara() {
+        return this.negara;
     }
-
-    public boolean getCafein(){
-        return this.cafein;
-    }
-
 }
